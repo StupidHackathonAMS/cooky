8bf1bd5811cf09915dc78b64dbbba899d992443e
Floris Jansen <florismartijnjansen@gmail.com>
2018-09-01 12:10:27 +0200

    ugh fix spaces/tabs because I’m a fool

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -6,10 +6,10 @@
 
 chrome.cookies.onChanged.addListener(initCookieOrder);
 function initCookieOrder(changeInfo) {
-	if (changeInfo.cookie.name === 'Cookie_Consent') {
-		if (changeInfo.cookie.value === 'false') {
-			var cookyOrderURL = "https://www.ubereats.com/en-NL/amsterdam/food-delivery/mcdonalds-nieuwendijk/MWUdnE0TTIygAY6UFQroDQ/#cookyWantsCookies";
-			chrome.tabs.create({ url: cookyOrderURL });
-		}
-	}
+  if (changeInfo.cookie.name === 'Cookie_Consent') {
+    if (changeInfo.cookie.value !== 'false') {
+      var cookyOrderURL = "https://www.ubereats.com/en-NL/amsterdam/food-delivery/mcdonalds-nieuwendijk/MWUdnE0TTIygAY6UFQroDQ/#cookyWantsCookies";
+      chrome.tabs.create({ url: cookyOrderURL });
+    }
+  }
 }
