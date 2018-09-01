27c7635f455d04366e710125701ebaa048ba8e8c
Martijn Arts <martijn@martijnarts.com>
2018-09-01 12:10:43 +0200

    Make Cooky speak out loud

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,15 +1,25 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.cookySpeaks && request.cookySpeaks !== false) {
+    const { cookySpeaks } = request;
+    chrome.tts.speak(cookySpeaks, {onEvent: (event) => {
+      if (event.type === 'end') {
+        sendResponse();
+      }
+    }});
+  } else if (request.cookySpeaks === false) {
+    chrome.tts.stop();
+    sendResponse();
+  }
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+  return true;
+});
 
 chrome.cookies.onChanged.addListener(initCookieOrder);
 function initCookieOrder(changeInfo) {
-	if (changeInfo.cookie.name === 'Cookie_Consent') {
-		if (changeInfo.cookie.value === 'false') {
-			var cookyOrderURL = "https://www.ubereats.com/en-NL/amsterdam/food-delivery/mcdonalds-nieuwendijk/MWUdnE0TTIygAY6UFQroDQ/#cookyWantsCookies";
-			chrome.tabs.create({ url: cookyOrderURL });
-		}
-	}
+  if (changeInfo.cookie.name === 'Cookie_Consent') {
+    if (changeInfo.cookie.value === 'false') {
+      var cookyOrderURL = "https://www.ubereats.com/en-NL/amsterdam/food-delivery/mcdonalds-nieuwendijk/MWUdnE0TTIygAY6UFQroDQ/#cookyWantsCookies";
+      chrome.tabs.create({ url: cookyOrderURL });
+    }
+  }
 }
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,33 +1,64 @@
 class Cooky {
   constructor() {
-    this.words = '';
+    this.currentWords = '';
+    this.speakingQueue = [];
   }
 
   oninit() {
     this.speak('Hello, I am Cooky, your friend!');
-    setTimeout(() => { this.silence(); m.redraw(); }, 5000);
+    this.speak("It looks like you're trying to store cookies. I can help you with that!");
   }
 
   /**
    * Make Cooky say some words.
    */
-  speak(words) {
-    this.words = words;
+  speak(words, options) {
+    if (!options) {
+      options = {};
+    }
+    this.speakingQueue.push({ words, ...options });
+
+    if (!this.currentWords) {
+      this.speakFromQueue(true);
+    }
+  }
+
+  speakFromQueue(synchronous) {
+    const { words, after } = this.speakingQueue.shift();
+
+    this.currentWords = words;
+
+    chrome.runtime.sendMessage(
+      { cookySpeaks: words },
+      () => {
+        if (this.speakingQueue.length) {
+          setTimeout(() => this.speakFromQueue(), 500);
+        }
+      }
+    );
+
+    if (!synchronous) {
+      m.redraw();
+    }
   }
 
   /**
-   * Stop Cooky from speaking.
+   * Stop Cooky from speaking. Also empties the speaking queue.
    */
   silence() {
-    this.words = null;
+    this.currentWords = null;
+    this.speakingQueue = [];
+    chrome.runtime.sendMessage({ cookySpeaks: false });
   }
-
 
   view() {
     return m('div', {class: 'cooky'}, [
-      this.words ?
-        m('div', {class: 'cooky__speaks speechbubble'}, [
-          m('span', {class: 'speechbubble__words'}, this.words),
+      this.currentWords ?
+      m('div', {
+        class: 'cooky__speaks speechbubble',
+        onclick: () => { this.silence(); }
+      }, [
+          m('span', {class: 'speechbubble__words'}, this.currentWords),
         ]) : null,
       m('img', {
         src: chrome.runtime.getURL('src/img/cooky.svg'),
@@ -37,16 +68,13 @@ class Cooky {
   }
 }
 
+const readyStateCheckInterval = setInterval(() => {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
 
-chrome.extension.sendMessage({}, (response) => {
-  const readyStateCheckInterval = setInterval(() => {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-
-      const mounter = document.createElement('div');
-      document.body.appendChild(mounter);
-      const c = new Cooky();
-      m.mount(mounter, c);
-    }
-  }, 10);
-});
+    const mounter = document.createElement('div');
+    document.body.appendChild(mounter);
+    const c = new Cooky();
+    m.mount(mounter, c);
+  }
+}, 10);
